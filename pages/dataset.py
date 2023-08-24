db978ec1dca16f5f0165e8fc841254dacbe6b8c3
bryanpiguave <bpiguave@espol.edu.ec>
2023-08-24 16:17:10 -0500

    adding dataset description

--- pages/dataset.py
+++ pages/dataset.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.markdown("Dataset")
 st.sidebar.markdown("# Dataset ")
+st.write("""The dataset has been processed to avoid outliers and duplicates. 
+         To accomplish that we make use of Fastdup""")
